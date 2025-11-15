dcb70d66a314e61b14e34d70f81a07675de46e28
David Hua <tkrd1205@gmail.com>
2025-11-15 21:13:38 +0800

    feat(alt_alloc): added allocators

--- arceos/modules/bump_allocator/src/lib.rs
+++ arceos/modules/bump_allocator/src/lib.rs
@@ -1,6 +1,7 @@
 #![no_std]
 
-use allocator::{BaseAllocator, ByteAllocator, PageAllocator};
+use allocator::{AllocError, BaseAllocator, ByteAllocator, PageAllocator};
+use core::ptr::NonNull;
 
 /// Early memory allocator
 /// Use it before formal bytes-allocator and pages-allocator can work!
@@ -16,21 +17,45 @@ use allocator::{BaseAllocator, ByteAllocator, PageAllocator};
 /// When it goes down to ZERO, free bytes-used area.
 /// For pages area, it will never be freed!
 ///
-pub struct EarlyAllocator<const SIZE: usize> {}
+pub struct EarlyAllocator<const SIZE: usize> {
+    start: usize,
+    end: usize,
+    b_pos: usize,
+    p_pos: usize,
+    b_count: usize,
+}
 
 impl<const SIZE: usize> EarlyAllocator<SIZE> {
     pub const fn new() -> Self {
-        Self {}
+        Self {
+            start: 0,
+            end: 0,
+            b_pos: 0,
+            p_pos: 0,
+            b_count: 0,
+        }
+    }
+
+    fn check_space(&self, required_size: usize, is_byte_alloc: bool) -> bool {
+        if is_byte_alloc {
+            self.b_pos.saturating_add(required_size) <= self.p_pos
+        } else {
+            self.p_pos.saturating_sub(required_size) >= self.b_pos
+        }
     }
 }
 
 impl<const SIZE: usize> BaseAllocator for EarlyAllocator<SIZE> {
     fn init(&mut self, start: usize, size: usize) {
-        todo!()
+        self.start = start;
+        self.end = start.checked_add(size).unwrap_or(0);
+        self.b_pos = self.start;
+        self.p_pos = self.end;
+        self.b_count = 0;
     }
 
     fn add_memory(&mut self, start: usize, size: usize) -> allocator::AllocResult {
-        todo!()
+        Err(AllocError::NoMemory)
     }
 }
 
@@ -39,23 +64,40 @@ impl<const SIZE: usize> ByteAllocator for EarlyAllocator<SIZE> {
         &mut self,
         layout: core::alloc::Layout,
     ) -> allocator::AllocResult<core::ptr::NonNull<u8>> {
-        todo!()
+        let aligned_b_pos = (self.b_pos + layout.align() - 1) & !(layout.align() - 1);
+        let new_b_pos = match aligned_b_pos.checked_add(layout.size()) {
+            Some(b) => b,
+            None => return Err(AllocError::NoMemory),
+        };
+
+        if new_b_pos > self.p_pos {
+            return Err(AllocError::NoMemory);
+        }
+
+        self.b_pos = new_b_pos;
+        self.b_count += 1;
+        Ok(unsafe {NonNull::new_unchecked(aligned_b_pos as *mut u8)})
     }
 
     fn dealloc(&mut self, pos: core::ptr::NonNull<u8>, layout: core::alloc::Layout) {
-        todo!()
+        let ptr = pos.as_ptr() as usize;
+        if ptr + layout.size() == self.b_pos {
+            self.b_pos = ptr;
+        }
     }
 
     fn total_bytes(&self) -> usize {
-        todo!()
+        self.end.saturating_sub(self.start)
     }
 
     fn used_bytes(&self) -> usize {
-        todo!()
+        let bytes_used = self.b_pos.saturating_sub(self.start);
+        let pages_used = self.end.saturating_sub(self.p_pos);
+        bytes_used + pages_used
     }
 
     fn available_bytes(&self) -> usize {
-        todo!()
+        self.p_pos.saturating_sub(self.b_pos)
     }
 }
 
@@ -67,22 +109,30 @@ impl<const SIZE: usize> PageAllocator for EarlyAllocator<SIZE> {
         num_pages: usize,
         align_pow2: usize,
     ) -> allocator::AllocResult<usize> {
-        todo!()
+        let size = num_pages.checked_mul(Self::PAGE_SIZE).ok_or(AllocError::NoMemory)?;
+        let unaligned_p_pos = self.p_pos.saturating_sub(size);
+        let align_mask = align_pow2 - 1;
+        let aligned_new_p_pos = unaligned_p_pos & !align_mask;
+
+        if aligned_new_p_pos < self.b_pos {
+            return Err(AllocError::NoMemory);
+        }
+
+        self.p_pos = aligned_new_p_pos;
+        Ok(self.p_pos)
     }
 
-    fn dealloc_pages(&mut self, pos: usize, num_pages: usize) {
-        todo!()
-    }
+    fn dealloc_pages(&mut self, pos: usize, num_pages: usize) {}
 
     fn total_pages(&self) -> usize {
-        todo!()
+        self.total_bytes() / Self::PAGE_SIZE
     }
 
     fn used_pages(&self) -> usize {
-        todo!()
+        self.end.saturating_sub(self.p_pos) / Self::PAGE_SIZE
     }
 
     fn available_pages(&self) -> usize {
-        todo!()
+        self.available_bytes() / Self::PAGE_SIZE
     }
 }
